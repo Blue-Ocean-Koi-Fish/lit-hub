6cde0b6488ae04d66b6703e3c7093935fb03d224
Jake <Jakereid53@gmail.com>
2022-06-25 12:46:45 -0600

    more text to speach testing

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  // on mouse enter say the inner text
+  const [time, setTime] = useState(false);
   const msg = new SpeechSynthesisUtterance();
-  const speeehHandler = (e) => {
-    msg.text = e.target.innerText;
-    window.speechSynthesis.speak(msg);
+  const speeehHandler = (target) => {
+    msg.text = target;
+    if (!time) {
+      window.speechSynthesis.speak(msg);
+      setTime(true);
+      setTimeout(() => {
+        setTime(false);
+      }, '500');
+    }
   };
   return (
     <div>
-      <h1 onMouseEnter={(e) => { console.log(e.target.innerText); speeehHandler(e); }}>
+      <h1 style={{ width: '30%' }} onMouseEnter={(e) => { speeehHandler(e.target.innerText); }}>
         Koi Fish!
       </h1>
+      <div style={{ width: '30%' }} onMouseEnter={() => { speeehHandler('Book Title'); }}>
+        <h2>Book Title</h2>
+        <div name="Book Title">Book Cover</div>
+      </div>
     </div>
   );
 }
