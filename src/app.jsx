58500076f88e79e112369961739c89f65cf24900
Jake <Jakereid53@gmail.com>
2022-06-27 08:55:23 -0600

    make speach moduler

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Speech, pause, cancel } from './speech';
 
 function App() {
   // on mouse enter say the inner text
-  const [time, setTime] = useState(false);
-  const msg = new SpeechSynthesisUtterance();
-  const speeehHandler = (target) => {
-    msg.text = target;
-    if (!time) {
-      window.speechSynthesis.speak(msg);
-      setTime(true);
-      setTimeout(() => {
-        setTime(false);
-      }, '500');
+  let paused = false;
+  document.addEventListener('keypress', (event) => {
+    if (event.code === 'Space') {
+      event.preventDefault();
+      pause(paused);
+      paused = !paused;
     }
-  };
+  }, false);
   return (
     <div>
-      <h1 style={{ width: '30%' }} onMouseEnter={(e) => { speeehHandler(e.target.innerText); }}>
+      <h1 style={{ width: '30%' }} onMouseEnter={(e) => { Speech(e.target.innerText); }} onMouseLeave={() => { cancel(); }}>
         Koi Fish!
       </h1>
-      <div style={{ width: '30%' }} onMouseEnter={() => { speeehHandler('Book Title'); }}>
+      <div style={{ width: '30%' }} onMouseEnter={() => { Speech('Book Title'); }} onMouseLeave={() => { cancel(); }}>
         <h2>Book Title</h2>
         <div name="Book Title">Book Cover</div>
       </div>
+      <p onMouseEnter={(e) => { Speech(e.target.innerText); }}>
+        a distinct section of a piece of writing,
+        usually dealing with a single theme and indicated by a new line, indentation, or numbering.
+        the concluding paragraph
+      </p>
     </div>
   );
 }
