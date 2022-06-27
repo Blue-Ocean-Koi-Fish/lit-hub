f03cfdee887ad782f0e45680da7a437ea0fe327d
Jake <Jakereid53@gmail.com>
2022-06-27 09:07:15 -0600

    Add sudo code for use on tts

--- src/speech.js
+++ src/speech.js
@@ -1,13 +1,22 @@
+// add to a onMouseEnter statment and pass in what you want to be said
 function Speech(target) {
   const msg = new SpeechSynthesisUtterance();
   msg.text = target;
   speechSynthesis.speak(msg);
 }
 
+// add to a onMouseLeave statment to stop tts when not hovering.
 function cancel() {
   speechSynthesis.cancel();
 }
 
+// use this function to set up pauseing on spacebar press.
+// document.addEventListener('keypress', (event) => {
+//  if (event.key === 'Space') {
+//    event.preventDefault();
+//    pause();
+//  }
+// }, false);
 function pause(test) {
   if (!test) {
     speechSynthesis.pause();
