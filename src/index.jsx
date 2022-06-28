e75371821116d851aa011d6b8f65a433b779772c
LWcaveman <levi_82@hotmail.com>
2022-06-28 15:05:25 -0500

    updated src-sw.js with workbox router and index with serviceworker listener

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,34 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Workbox } from 'workbox-window';
 import App from './app';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
+    const wb = new Workbox('/sw.js');
+    const updateButton = document.querySelector("#update");
+    wb.addEventListener('waiting', (event) => {
+      console.log(
+        `A new service worker has installed, but it can't activate
+          until all tabs running the current version have fully unloaded.`,
+      );
+      updateButton.classList.add('show');
+      updateButton.addEventListener('click', () => {
+      // Set up a listener that will reload the page as soon as the previously
+      // waiting service worker has taken control.
+        wb.addEventListener("controlling", (event) => {
+          window.location.reload();
+        });
+        // Send a message telling the service worker to skip waiting.
+        // This will trigger the `controlling` event handler above.
+        wb.messageSW({ type: 'SKIP_WAITING' });
+      });
+    });
+
+    wb.register();
   });
 }
 
