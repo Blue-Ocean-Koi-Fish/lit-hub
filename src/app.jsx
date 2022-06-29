eeffd8fb12321a6408040ef2d4511ea830d597ca
Justin C Kirk <justin.c.kirk@gmail.com>
2022-06-29 10:03:45 -0400

    adds reader function

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Login from './login';
-// import Collection from "./collection";
 import Settings from "./settings";
-// import Reader from "./reader";
 import Header from "./header";
-
-
 import SearchDisplay from "./search/searchdisplay";
 import SearchSection from "./search/searchsection";
+
+// import Collection from "./collection";
+// import Reader from "./reader";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(true);
