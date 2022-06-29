ec6725eca5f8366b643412f1ae32eb2840630eeb
Davyd Zakorchennyi <Diza41a@mail.com>
2022-06-29 13:11:34 -0400

    Fixed login styling (simply class name tinkering)

--- src/login.jsx
+++ src/login.jsx
@@ -6,27 +6,35 @@ function Login({ setLoggedIn }) {
 
   return (
     <section className="login-section">
+
       <div className="hero">
         bubbly-animation
       </div>
+
       <div className="login-wrap">
         <div className="logo-wrap">
-          <h1 className="title">
-            LitHub
-          </h1>
-          <h3 className="subtitle">
-            Web
-          </h3>
+          <div className="logo-text">
+            <h1 className="title">
+              <span className="capital">L</span>
+              it
+              <span className="capital">H</span>
+              ub
+            </h1>
+            <h3 className="subtitle">Web</h3>
+          </div>
+
           <div className="img" alt="logo">
             {/* <img src="http://placecorgi.com/260/180" alt="logo" /> */}
           </div>
         </div>
+
         <form className="login-form">
           <h4 className="welcome-msg">
             Welcome to LitHub! Please Login or Register:
           </h4>
           <div className="input-wrap">
             {/* <img src="http://placecorgi.com/50/50" alt="username" /> */}
+            <i className="fa-solid fa-user icon" />
             <input
               id="username"
               type="text"
@@ -37,6 +45,7 @@ function Login({ setLoggedIn }) {
           </div>
           <div className="input-wrap">
             {/* <img src="http://placecorgi.com/50/50" alt="password" /> */}
+            <i className="fa-solid fa-lock icon" />
             <input
               id="password"
               type="password"
