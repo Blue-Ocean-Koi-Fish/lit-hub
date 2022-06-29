5d6ae3a2e3dab6be91a8d1d03b5fb78c804a2842
Cory Nickerson <essej4269@Hotmail.com>
2022-06-29 08:38:59 -0400

    Search and search display integrated with app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,49 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import Login from './login';
-import Collection from './collection';
-import SearchResults from './searchResults';
-import Settings from './settings';
-import Reader from './reader';
-import Header from './header';
-import SearchForm from './searchForm';
+import React, { useState, useEffect } from "react";
+import Login from "./login";
+import Collection from "./collection";
+import SearchDisplay from "./search/searchdisplay";
+import Settings from "./settings";
+import Reader from "./reader";
+import Header from "./header";
+import SearchSection from "./search/searchsection";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [searchTerms, setSearchTerms] = useState({
-    'book-name': '',
-    author: '',
-    language: '',
-    genre: '',
+    title: "",
+    author: "",
+    language: "",
+    topic: "",
   });
+  const [userBooks, setUserBooks] = useState([]);
+  const [bookList, setBookList] = useState();
+  const [count, setCount] = useState(0);
   const [settings, setSettings] = useState({
-    language: 'english',
-    'color-blindedness': 'none',
+    language: "english",
+    "color-blindedness": "none",
   });
   const [showSearchResults, setShowSearchResults] = useState(false);
   const [showSettings, setShowSettings] = useState(false);
   const [showReader, setShowReader] = useState(false);
-
+  console.log(userBooks);
   return (
     <div id="root">
       {loggedIn ? (
         <>
-          <Header setShowSettings={setShowSettings} setShowSearchResults={setShowSearchResults} />
-          <SearchForm
+          <Header
+            setShowSettings={setShowSettings}
+            setShowSearchResults={setShowSearchResults}
+          />
+          <SearchSection
+            setCount={setCount}
             searchTerms={searchTerms}
             setSearchTerms={setSearchTerms}
             setShowSearchResults={setShowSearchResults}
+            setBookList={setBookList}
           />
-          {showSearchResults
-            ? <SearchResults searchTerms={searchTerms} setSearchTerms={setSearchTerms} />
-            : <Collection />}
+          {showSearchResults ? (
+            <SearchDisplay
+              setUserBooks={setUserBooks}
+              searchTerms={searchTerms}
+              setSearchTerms={setSearchTerms}
+              count={count}
+              bookList={bookList}
+            />
+          ) : (
+            <Collection />
+          )}
           {showSettings ? (
             <Settings
               settings={settings}
               setSettings={setSettings}
               setShowSettings={setShowSettings}
             />
-          )
-            : null}
+          ) : null}
           {showReader ? <Reader /> : null}
         </>
       ) : (
