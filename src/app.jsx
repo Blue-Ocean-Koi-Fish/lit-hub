8b1620986cc322bb42b0c39d32aec43799e673a2
CamEstep <camestep8517@gmail.com>
2022-06-29 20:32:35 -0400

    Login / Register User WIP

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import Login from './login';
-import Settings from "./settings";
-import Header from "./header";
-import SearchDisplay from "./search/searchdisplay";
-import SearchSection from "./search/searchsection";
+import Settings from './settings';
+import Header from './header';
+import SearchDisplay from './search/searchdisplay';
+import SearchSection from './search/searchsection';
 
-import Collection from "./collection";
+const axios = require('axios');
+
+import Collection from './collection';
 // import Reader from "./reader";
 
 import '../public/styles/unified.css';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
   const [searchTerms, setSearchTerms] = useState({
     title: "",
     author: "",
@@ -27,8 +29,21 @@ function App() {
   });
   const [showSearchResults, setShowSearchResults] = useState(false);
   const [showSettings, setShowSettings] = useState(false);
-
   const [showReader, setShowReader] = useState(true);
+
+  useEffect(() => {
+    if (document.cookie) {
+      axios.post('http://localhost:8080/verifyToken', { token: document.cookie })
+        .then((res) => {
+          console.log(res);
+          setLoggedIn(true);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  });
+
   return (
     loggedIn ? (
       <>
