58ee3bcc0499b6ddb07bbd5a1aecee42b42e45f9
Jake <Jakereid53@gmail.com>
2022-06-30 09:24:40 -0600

    Implement reeder tts and button

--- src/reader/speech.js
+++ src/reader/speech.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-loop-func */
 // add to a onMouseEnter statment and pass in what you want to be said
 function Speech(target, lan) {
   const msg = new SpeechSynthesisUtterance();
   msg.text = target;
   msg.lang = lan || 'en-US';
   speechSynthesis.speak(msg);
+  return msg;
 }
 
 // add to a onMouseLeave statment to stop tts when not hovering.
@@ -18,51 +20,85 @@ function pause(test) {
     speechSynthesis.resume();
   }
 }
-
-// use this function to set up pauseing on spacebar press.
+cancel();
 let started = false;
 let paused = false;
+
+const startText = () => {
+  if (started) {
+    pause(paused);
+    // eslint-disable-next-line no-unused-expressions
+    paused ? document.getElementById('tts').textContent = 'Pause Reading' : document.getElementById('tts').textContent = 'Start Reading';
+    paused = !paused;
+  } else {
+    started = true;
+    document.getElementById('tts').textContent = 'Pause Reading';
+    // started = read(document.getElementById('big'));
+    const doc = document.getElementById('content');
+    for (let i = 0; i < doc.children.length; i += 1) {
+      Speech(doc.children[i].innerText)
+        .onend = () => {
+          if (i === doc.children.length - 1) {
+            started = false;
+            document.getElementById('tts').textContent = 'Start Reading';
+          }
+        };
+    }
+  }
+};
+
 document.addEventListener('keypress', (event) => {
-  console.log('DOCUMENT EVENT LISTENER');
   if (event.code === 'Space') {
     event.preventDefault();
-    if (started) {
-      pause(paused);
-      paused = !paused;
-    } else {
-      Speech(document.getElementById('test').innerText)
-        .onend = () => {
-          started = false;
-        };
-      started = true;
-    }
+    // eslint-disable-next-line no-use-before-define
+    startText();
   }
 }, false);
 
-//code for the reader
-document.addEventListener('keypress', (event) => {
-  if (event.code === 'Space') {
-    event.preventDefault();
-    if (started) {
-      pause(paused);
-      paused = !paused;
-    } else {
-      started = true;
-      // started = read(document.getElementById('big'));
-      const doc = document.getElementById('big')
-      for (let i = 0; i < doc.children.length; i++) {
-        Speech(doc.children[i].innerText)
-          // eslint-disable-next-line no-loop-func
-          .onend = () => {
-            if (i === doc.children.length - 1) {
-              started = false;
-            }
-          };
-      }
-    }
-  }
-}, false);
+// use this function to set up pauseing on spacebar press.
+// let started = false;
+// let paused = false;
+// // document.addEventListener('keypress', (event) => {
+// //   console.log('DOCUMENT EVENT LISTENER');
+// //   if (event.code === 'Space') {
+// //     event.preventDefault();
+// //     if (started) {
+// //       pause(paused);
+// //       paused = !paused;
+// //     } else {
+// //       Speech(document.getElementById('test').innerText)
+// //         .onend = () => {
+// //           started = false;
+// //         };
+// //       started = true;
+// //     }
+// //   }
+// // }, false);
+
+// // code for the reader
+// document.addEventListener('keypress', (event) => {
+//   if (event.code === 'Space') {
+//     event.preventDefault();
+//     if (started) {
+//       pause(paused);
+//       paused = !paused;
+//     } else {
+//       started = true;
+//       // started = read(document.getElementById('big'));
+//       const doc = document.getElementById('big');
+//       for (let i = 0; i < doc.children.length; i += 1) {
+//         Speech(doc.children[i].innerText)
+//           // eslint-disable-next-line no-loop-func
+//           .onend = () => {
+//             if (i === doc.children.length - 1) {
+//               started = false;
+//             }
+//           };
+//       }
+//     }
+//   }
+// }, false);
 
 export {
-  Speech, pause, cancel,
+  Speech, pause, cancel, startText,
 };
